ccfd5a131913fc4ac3c4c0383b65c59430d6d87d
Daniel Burnley <dan@danops.uk>
2018-10-09 11:05:32 +0100

    Add failing acceptance spec for new chirps

---
+++ spec/acceptance/chirper_spec.rb
@@ -0,0 +1,24 @@
+describe "Creating dem chirps" do
+  let(:chirp_file) { '/tmp/chirps.json' }
+
+  before do
+    File.new(chirp_file, 'w') {}
+  end
+
+  after do
+    File.delete(chirp_file)
+  end
+
+  it "allows me to post and view chirps" do
+    chirp_gateway = Chirper::Gateway::FileChirp.new(file_path: '/tmp/chirps.json')
+    create_new_chirps = Chirper::UseCase::PostChirp.new(chirp_gateway: chirp_gateway)
+    view_chirps = Chirper::UseCase::ViewChirps.new(chirp_gateway: chirp_gateway)
+
+    create_new_chirps.execute(username: 'Test One', chirp: 'I just want to pet all the cats.')
+    create_new_chirps.execute(username: 'Test Two', chirp: 'All dogs are good dogs.')
+
+    chirps = view_chirps.execute
+    expect(chirps[0]).to eq({username: 'Test One', chirp: 'I just want to pet all the cats.'})
+    expect(chirps[1]).to eq({username: 'Test Two', chirp: 'All dogs are good dogs.'})
+  end
+end
